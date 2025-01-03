ce2656f4d3a150baa9700eea07948204bd235524
shamil <shamilnk0458@gmail.com>
2025-01-03 19:56:50 +0530

    Updated Owner-side forgot password UI

--- frontend/mandavu/src/Components/Owner/OwnerSetNewPasswordCmp.jsx
+++ frontend/mandavu/src/Components/Owner/OwnerSetNewPasswordCmp.jsx
@@ -4,85 +4,109 @@ import { toast } from "react-toastify";
 import * as Yup from "yup";
 import { useNavigate } from "react-router-dom";
 import { axiosOwnerInstance } from "../../Utils/Axios/axiosInstance";
+import { useState } from "react";
+import { CircularProgress } from "@mui/material";
 
 
-export default function OwnerSetNewPasswordCmp({uidb64, token}) {
-    const navigate = useNavigate();
-    const formik = useFormik({
-        initialValues: {
-          password: '',
-          confirm_password: '',
-          uidb64: uidb64 || '',
-          token: token || '',
-        },
-        validationSchema: Yup.object({
-          password: Yup.string()
-            .min(8, 'Password must be at least 8 characters long')
-            .required('Password is required'),
-          confirm_password: Yup.string()
-            .oneOf([Yup.ref('password'), null], 'Passwords must match')
-            .required('Please confirm your password'),
-        }),
-        onSubmit: async (values) => {
-          console.log(values);
-          try{
-            const response = await axiosOwnerInstance.patch('owner-setnew-password/', values);
-            toast.success(response.data.message);
-            navigate('/owner/login')
-          } catch (error) {
-            toast.error('Failed to create new password. Please try again later.');
-          }
-        },
-      });
-    
-   return(
-    <div className="min-h-screen bg-teal-600 flex justify-center items-center">
-    <div className="absolute w-60 h-60 rounded-xl bg-teal-500 -top-5 -left-16 z-0 transform rotate-45 hidden md:block"></div>
-    <div className="py-12 px-12 bg-white rounded-2xl shadow-xl z-20">
-      <div>
-        <h1 className="text-3xl font-bold text-center mb-4 cursor-pointer">Reset Your Password</h1>
-        <p className="w-80 text-center text-sm mb-8 font-semibold text-gray-700 tracking-wide cursor-pointer">
-          Please enter your new password and confirm it below.
-        </p>
-      </div>
-      <form onSubmit={formik.handleSubmit} className="space-y-4">
-        <input
-          type="password"
-          name="password"
-          id="password"
-          value={formik.values.password}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          placeholder="Enter your new password"
-          className="block text-sm py-3 px-4 rounded-lg w-full bg-white border border-gray-300 outline-teal-500"
-        />
-        {formik.errors.password && formik.touched.password ? (
-          <div className="text-red-500 text-sm">{formik.errors.password}</div>
-        ) : null}
 
-        <input
-          type="password"
-          name="confirm_password"
-          id="confirm_password"
-          value={formik.values.confirm_password}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          placeholder="Confirm your new password"
-          className="block text-sm py-3 px-4 rounded-lg w-full bg-white border border-gray-300 outline-teal-500"
-        />
-        {formik.errors.confirm_password && formik.touched.confirm_password ? (
-          <div className="text-red-500 text-sm">{formik.errors.confirm_password}</div>
-        ) : null}
+export default function OwnerSetNewPasswordCmp({ uidb64, token }) {
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false)
 
-        <div className="text-center mt-6">
-          <button type="submit" className="w-1/2 py-2 text-xl text-white bg-teal-500 rounded-lg hover:bg-teal-600 transition-all">
-            Submit
-          </button>
+  const formik = useFormik({
+    initialValues: {
+      password: '',
+      confirm_password: '',
+      uidb64: uidb64 || '',
+      token: token || '',
+    },
+    validationSchema: Yup.object({
+      password: Yup.string()
+        .min(8, 'Password must be at least 8 characters long')
+        .required('Password is required'),
+      confirm_password: Yup.string()
+        .oneOf([Yup.ref('password'), null], 'Passwords must match')
+        .required('Please confirm your password'),
+    }),
+    onSubmit: async (values) => {
+      setLoading(true);
+      try {
+        const response = await axiosOwnerInstance.patch('owner-setnew-password/', values);
+        toast.success(response.data.message);
+        navigate('/owner/login')
+      } catch (error) {
+        toast.error('Failed to create new password. Please try again later.');
+      }finally{
+        setLoading(false);
+      }
+    },
+  });
+
+  return (
+    <div className="min-h-screen bg-teal-600 flex justify-center items-center px-4 sm:px-6">
+      <div className="py-8 px-4 sm:px-12 bg-white rounded-2xl shadow-xl z-20 w-full max-w-lg">
+
+        <div className="flex justify-center mb-6">
+          <img
+            src="/user/mandavu-logo.png"
+            alt="Mandavu Logo"
+            className="w-24 h-auto sm:w-32"
+          />
         </div>
-      </form>
+
+
+        <div>
+          <h1 className="text-xl font-semibold text-gray-700 text-center mb-2 cursor-pointer">Reset Your Password</h1>
+          <p className="w-full text-center text-sm mb-8 font-semibold text-gray-600 tracking-wide cursor-pointer">
+            Please enter your new password and confirm it below.
+          </p>
+        </div>
+        <form onSubmit={formik.handleSubmit}>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={formik.values.password}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            placeholder="Enter your new password"
+            className="block text-sm py-3 px-4 mt-2 rounded-lg w-full bg-white border border-gray-300 outline-teal-500"
+          />
+          {formik.errors.password && formik.touched.password ? (
+            <div className="text-red-500 text-sm">{formik.errors.password}</div>
+          ) : null}
+
+          <input
+            type="password"
+            name="confirm_password"
+            id="confirm_password"
+            value={formik.values.confirm_password}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            placeholder="Confirm your new password"
+            className="block text-sm py-3 px-4 mt-4 rounded-lg w-full bg-white border border-gray-300 outline-teal-500"
+          />
+          {formik.errors.confirm_password && formik.touched.confirm_password ? (
+            <div className="text-red-500 text-sm">{formik.errors.confirm_password}</div>
+          ) : null}
+
+          <div className="text-center mt-8">
+            <button
+              type="submit"
+              disabled={loading}
+              className={`w-1/2 py-2 text-lg text-white bg-teal-600 rounded-lg hover:bg-teal-800 transition-all duration-300 ${loading ? 'cursor-not-allowed opacity-70' : ''}`}
+            >
+              {loading ? (
+                <CircularProgress size={20} style={{ color: 'white' }} />
+              ) : (
+                'Submit'
+              )}
+
+            </button>
+          </div>
+        </form>
+      </div>
+
     </div>
-    <div className="w-40 h-40 absolute bg-teal-500 rounded-full top-0 right-12 hidden md:block"></div>
-    <div className="w-20 h-40 absolute bg-teal-500 rounded-full bottom-20 left-10 transform rotate-45 hidden md:block"></div>
-  </div>
-   )
+  )
 }
--- frontend/mandavu/src/Pages/Owner/OwnerResetPasswordRequest.jsx
+++ frontend/mandavu/src/Pages/Owner/OwnerResetPasswordRequest.jsx
@@ -3,62 +3,91 @@ import { useFormik } from "formik";
 import { toast } from "react-toastify";
 import * as Yup from "yup";
 import { axiosOwnerInstance } from "../../Utils/Axios/axiosInstance";
+import { CircularProgress } from "@mui/material";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 
 
 export default function OwnerResetPasswordRequest() {
-    const formik = useFormik({
-        initialValues: {
-          email: '',
-        },
-        validationSchema: Yup.object({
-          email: Yup.string().email('Invalid email address').required('Email is required'),
-        }),
-        onSubmit: async (values) => {
-          console.log(values);
-    
-          try{
-            const response = await axiosOwnerInstance.post('owner-password-reset-request/',values);
-            toast.success(response.data.message)
-          }catch(error) {
-            toast.error('Failed to send password reset link.')
-          }
-        },
-      });
-    return(
-        <div className="min-h-screen bg-teal-600 flex justify-center items-center">
-        <div className="absolute w-60 h-60 rounded-xl bg-teal-500 -top-5 -left-16 z-0 transform rotate-45 hidden md:block"></div>
-        <div className="py-12 px-12 bg-white rounded-2xl shadow-xl z-20">
-          <div>
-            <h1 className="text-3xl font-bold text-center mb-4 cursor-pointer">Enter Your Email</h1>
-            <p className="w-80 text-center text-sm mb-8 font-semibold text-gray-700 tracking-wide cursor-pointer">
-            A password reset request will be sent <br></br> Please check your email
-            </p>
-          </div>
-          <form onSubmit={formik.handleSubmit} className="space-y-4">
-            <input
-              type="email"
-              name="email"
-              id="email"
-              value={formik.values.email}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              placeholder="Enter your email"
-              className="block text-sm py-3 px-4 rounded-lg w-full bg-white border border-gray-300 outline-teal-500"
-            />
-            {formik.errors.email && formik.touched.email ? (
-              <div className="text-red-500 text-sm">{formik.errors.email}</div>
-            ) : null}
+
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate()
   
-            <div className="text-center mt-6">
-              <button type="submit" className="w-1/2 py-2 text-xl text-white bg-teal-500 rounded-lg hover:bg-teal-600 transition-all">
-                Submit
-              </button>
-            </div>
-          </form>
-          
+
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+    },
+    validationSchema: Yup.object({
+      email: Yup.string().email('Invalid email address').required('Email is required'),
+    }),
+    onSubmit: async (values) => {
+      setLoading(true);
+
+      try {
+        const response = await axiosOwnerInstance.post('owner-password-reset-request/', values);
+        navigate('/user/login')
+        toast.success(response.data.message)
+      } catch (error) {
+        toast.error('Failed to send password reset link.')
+      } finally {
+        setLoading(false)
+      }
+    },
+  });
+
+
+  return (
+    <div className="min-h-screen bg-teal-600 flex justify-center items-center px-4 sm:px-6">
+      <div className="py-8 px-4 sm:px-12 bg-white rounded-2xl shadow-xl z-20  w-full max-w-lg">
+
+        <div className="flex justify-center mb-4">
+          <img
+            src="/user/mandavu-logo.png"
+            alt="Mandavu Logo"
+            className="w-24 h-auto sm:w-32"
+          />
         </div>
-        <div className="w-40 h-40 absolute bg-teal-500 rounded-full top-0 right-12 hidden md:block"></div>
-        <div className="w-20 h-40 absolute bg-teal-500 rounded-full bottom-20 left-10 transform rotate-45 hidden md:block"></div>
+        <div>
+          <h1 className="text-xl font-semibold  text-gray-700 text-center mb-4 cursor-pointer">Enter Your Email</h1>
+          <p className="w-full text-center text-sm mb-8 font-semibold text-gray-600 tracking-wide cursor-pointer">
+            A password reset request will be sent <br></br> Please check your email
+          </p>
+        </div>
+        <form onSubmit={formik.handleSubmit} >
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            placeholder="Enter your email"
+            className="block text-sm py-3 px-4 rounded-lg w-full bg-white border border-gray-300 outline-teal-600"
+          />
+          {formik.errors.email && formik.touched.email ? (
+            <div className="text-red-500 text-sm ml-2 mt-2">{formik.errors.email}</div>
+          ) : null}
+
+          <div className="text-center mt-6">
+            <button
+              type="submit"
+              disabled={loading}
+              className={`w-1/2 py-2 text-lg text-white bg-teal-600 rounded-lg hover:bg-teal-800 transition-all duration-300 ${loading ? 'cursor-not-allowed opacity-70' : ''} `}
+            >
+              {loading ? (
+                <CircularProgress size={20} style={{ color: 'white' }} />
+              ) : (
+                'Submit'
+              )}
+
+            </button>
+          </div>
+        </form>
+
       </div>
-    )
+
+    </div>
+  )
 }
